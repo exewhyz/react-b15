bb0f2dd54b8c57f72cbf853567d00a180ad32f01
Aniket Raj <araj52444@gmail.com>
2025-12-22 09:15:47 +0000

    Component and Setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <h1>Hello from App</h1>
+  )
+}
+
+// const a = 10;
+
+export default App; //default export
+// export { a }; //named export
+
+// const App1 = () => {}
